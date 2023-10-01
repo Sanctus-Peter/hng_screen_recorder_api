793a35a4b5041343ce497226fc120d21c9e43b51
Sanctus-Peter <sanctuspeter@gmail.com>
2023-10-01 16:42:07 +0100

    update requirements

--- screen_record_backend/settings.py
+++ screen_record_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5iul+z^j2vj64e4)@hne9&60i9_y7)ijs^xee&gue5j-$2^)*t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
